5e2dd7c99fd0060f7ada61697eae380183af285a
Ryoji Nomura <ryoji_nomura@albert2005.co.jp>
2019-03-04 11:14:25 +0900

    test conn

--- matrix.cpp
+++ matrix.cpp
@@ -1,4 +1,7 @@
 #include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 
 int a[100][100];
 
@@ -6,6 +9,8 @@ int main(){
     for (int i=0; i<100; i++){
         for (int j=0; j<100; j++){
             a[i][j] = 1;
+            cout << a[i][j] << endl;
         }
     }
+    return 0;
 }
